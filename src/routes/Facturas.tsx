edc51ac5fac4f70c125170a66a461f3ee3d42f18
RamonUlises <jorgitomilina@gmail.com>
2025-07-05 20:30:23 -0600

    facturas por rango

--- src/routes/Facturas.tsx
+++ src/routes/Facturas.tsx
@@ -25,28 +25,33 @@ import { server } from "@/lib/server";
 export const Facturas = () => {
   const { facturas } = useFacturas();
   const [facturasSeleccionadas, setFacturasSeleccionadas] = useState(facturas);
-  const [date, setDate] = useState<Date | undefined>(undefined);
+  const [date1, setDate1] = useState<Date | undefined>(undefined);
+  const [date2, setDate2] = useState<Date | undefined>(undefined);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setFacturasSeleccionadas(facturas);
+    setDate1(undefined);
+    setDate2(undefined);
   }, [facturas]);
 
-  useEffect(() => {
-    setLoading(true);
-    if (!date) {
-      setFacturasSeleccionadas(facturas);
-      setLoading(false);
+  async function filterFacturas() {
+    if (!date1 || !date2) {
+      alert("Debes seleccionar un rango de fechas");
       return;
     }
 
-    const fechaFormateada = date.toISOString().split("T")[0];
-    searchFacturas(fechaFormateada);
-  }, [date]);
+    setLoading(true);
 
-  async function searchFacturas(fecha: string) {
+    const fechaFormateada1 = date1.toISOString().split("T")[0];
+    const fechaFormateada2 = date2.toISOString().split("T")[0];
+
+    searchFacturas(fechaFormateada1, fechaFormateada2);
+  }
+
+  async function searchFacturas(fecha1: string, fecha2: string) {
     try {
-      const response = await fetch(`${server.url}/facturas/fecha/${fecha}`, {
+      const response = await fetch(`${server.url}/facturas/rango/${fecha1}/${fecha2}`, {
         headers: {
           Authorization: `Basic ${server.credetials}`,
         },
@@ -68,87 +73,135 @@ export const Facturas = () => {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center gap-8 mt-4">
-        <h1 className="text-center font-bold text-zinc-800">Facturas</h1>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={"outline"}
-              className={cn(
-                "w-[280px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon />
-              {date ? (
-                new Date(date).toLocaleDateString()
-              ) : (
-                <span>Seleccione la fecha</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              initialFocus
-              locale={es}
-            />
-          </PopoverContent>
-        </Popover>
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 text-sm transition-colors duration-300"
-          onClick={() => setDate(undefined)}
-        >
-          eliminar filtro
-        </button>
+      <div className="flex items-center justify-between px-8 mt-4">
+        <div className="flex items-center gap-4">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                  !date1 && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon />
+                {date1 ? (
+                  new Date(date1).toLocaleDateString()
+                ) : (
+                  <span>Fecha inicio</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date1}
+                onSelect={setDate1}
+                initialFocus
+                locale={es}
+              />
+            </PopoverContent>
+          </Popover>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[200px] justify-start text-left font-normal",
+                  !date2 && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon />
+                {date2 ? (
+                  new Date(date2).toLocaleDateString()
+                ) : (
+                  <span>Fecha final</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date2}
+                onSelect={setDate2}
+                initialFocus
+                locale={es}
+              />
+            </PopoverContent>
+          </Popover>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 text-sm transition-colors duration-300"
+            onClick={() => {
+              filterFacturas();
+            }}
+          >
+            filtrar
+          </button>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 text-sm transition-colors duration-300"
+            onClick={() => {
+              setDate1(undefined);
+              setDate2(undefined);
+              setFacturasSeleccionadas(facturas);
+            }}
+          >
+            eliminar filtro
+          </button>
+        </div>
+        <div>
+          <p className="font-bold">Monto total:</p>
+          <p>
+            C${" "}
+            {facturasSeleccionadas
+              .reduce((acum, factura) => acum + factura.total, 0)
+              .toFixed(2)}
+          </p>
+        </div>
       </div>
 
-      {loading && (
-        <h1 className="text-center font-bold mt-4">
-          Cargando...
-        </h1>
-      )}
+      {loading && <h1 className="text-center font-bold mt-4">Cargando...</h1>}
       {facturasSeleccionadas.length === 0 && !loading ? (
         <h3 className="text-center mt-8 font-bold">Sin Facturas</h3>
-      ) : facturasSeleccionadas.length > 0 && !loading && (
-        <Tabs defaultValue="todas" className="w-full my-4 h-full">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="todas">Todas</TabsTrigger>
-            <TabsTrigger value="pagadas">Pagadas</TabsTrigger>
-            <TabsTrigger value="nopagadas">No pagadas</TabsTrigger>
-          </TabsList>
-          <TabsContent value="todas" className="w-full h-full">
-            <Card>
-              <CardContent className="space-y-2">
-                <FacturasMostar facturas={facturasSeleccionadas} />
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="pagadas" className="w-full h-full">
-            <Card>
-              <CardContent className="space-y-2">
-                <FacturasMostar
-                  facturas={facturasSeleccionadas.filter(
-                    (factura) => factura.tipo === "contado"
-                  )}
-                />
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="nopagadas" className="w-full h-full">
-            <Card>
-              <CardContent className="space-y-2">
-                <FacturasMostar
-                  facturas={facturasSeleccionadas.filter(
-                    (factura) => factura.tipo === "crédito"
-                  )}
-                />
-              </CardContent>
-            </Card>
-          </TabsContent>
-        </Tabs>
+      ) : (
+        facturasSeleccionadas.length > 0 &&
+        !loading && (
+          <Tabs defaultValue="todas" className="w-full my-4 h-full">
+            <TabsList className="grid w-full grid-cols-3">
+              <TabsTrigger value="todas">Todas</TabsTrigger>
+              <TabsTrigger value="pagadas">Pagadas</TabsTrigger>
+              <TabsTrigger value="nopagadas">No pagadas</TabsTrigger>
+            </TabsList>
+            <TabsContent value="todas" className="w-full h-full">
+              <Card>
+                <CardContent className="space-y-2">
+                  <FacturasMostar facturas={facturasSeleccionadas} />
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="pagadas" className="w-full h-full">
+              <Card>
+                <CardContent className="space-y-2">
+                  <FacturasMostar
+                    facturas={facturasSeleccionadas.filter(
+                      (factura) => factura.tipo === "contado"
+                    )}
+                  />
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="nopagadas" className="w-full h-full">
+              <Card>
+                <CardContent className="space-y-2">
+                  <FacturasMostar
+                    facturas={facturasSeleccionadas.filter(
+                      (factura) => factura.tipo === "crédito"
+                    )}
+                  />
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        )
       )}
     </Layout>
   );
