07d834db0134552a20d3f8bf6654074152c5ba32
RamonUlises <jorgitomilina@gmail.com>
2025-07-06 21:45:31 -0600

    Actualizar la versión a 3.1.0 en package.json, tauri.conf.json y lógica de actualización

--- src/utils/actualizacion.ts
+++ src/utils/actualizacion.ts
@@ -13,7 +13,7 @@ export async function actualizacion() {
     });
 
     const data: ActuType = await response.json();
-    const currentVersion = "3.0.0";
+    const currentVersion = "3.1.0";
 
     if (data.version !== currentVersion) {
       const userResponse = await confirm(
